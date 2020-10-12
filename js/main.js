a8ddd4540fcedc3ce6932cf8fd41c22f4f8dcac0
segitiga <rafi.nizar15@gmail.com>
2020-10-12 09:47:13 +0700

    added: 2 segitiga

--- js/main.js
+++ js/main.js
@@ -2,52 +2,71 @@ function main() {
     var canvas = document.getElementById("workCanvas");
     var gl = canvas.getContext("webgl");
 
-    // * def vertices
-    /**
-        * A (-0.5, 0.5); B (-0.5, -0.5); C (0.5, -0.5)
-    */
     var vertices = [
-        -0.5, 0.5,
-        -0.5, -0.5,
-         0.5, -0.5,
-         0.5, 0.5
+        -0.5, 0.5, 1.0, 0.0, 0.0, // Titik A 
+        -0.5, -0.5, 1.0, 0.0, 0.0, // Titik B
+        0.5, -0.5, 1.0, 0.0, 0.0, // Titik C
+        0.5, -0.5, 0.0, 0.0, 1.0, // Titik C
+        0.5, 0.5, 0.0, 0.0, 1.0, // Titik D
+        -0.5, 0.5, 0.0, 0.0, 1.0 // Titik A 
     ];
 
-    // vertex buffer object
     var vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  
+
     var vertexShaderCode = document.getElementById("vertexShaderSource").text;
-  
+
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
-  
+
     var fragmentShaderCode = document.getElementById("fragmentShaderSource").text;
-  
+
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
     gl.compileShader(fragmentShader);
-  
+
+    // Check the compile status
+    var compiled = gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS);
+    if (!compiled) {
+        // Something went wrong during compilation; get the error
+        console.error(gl.getShaderInfoLog(fragmentShader));
+    }
+
     var shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
-  
+
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    var aColor = gl.getAttribLocation(shaderProgram, "a_Color");
+    gl.vertexAttribPointer(
+        aPosition,
+        2,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        0);
+    gl.vertexAttribPointer(
+        aColor,
+        3,
+        gl.FLOAT,
+        false,
+        5 * Float32Array.BYTES_PER_ELEMENT,
+        2 * Float32Array.BYTES_PER_ELEMENT);
     gl.enableVertexAttribArray(aPosition);
-  
+    gl.enableVertexAttribArray(aColor);
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  
-    var primitive = gl.POINTS;
+    gl.viewport(100, 0, canvas.height, canvas.height);
+
+    var primitive = gl.TRIANGLES;
     var offset = 0;
-    var count = 4;
+    var count = 6; // Jumlah verteks yang akan digambar
     gl.drawArrays(primitive, offset, count);
 }
